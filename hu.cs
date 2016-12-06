515fa7ecc8fb657ca16faa89ef04513e5723e3c2
notoaki15 <siderop0ne0@gmail.com>
2016-12-06 18:48:16 +0900

    Create hu.cs

---
+++ hu.cs
@@ -0,0 +1,18 @@
+using System;
+namespace Application
+{
+	public static void Main(String[] args)
+	{
+		ReadLine RL = new ReadLine();
+		while(true)
+		{
+		Console.WriteLine(RL.ReadLine());
+			Console.WriteLine("こんにちは");
+		}
+
+	}
+	public override string ReadLine()
+	{
+		StreamReaer sr = new StreamReader();
+	}
+}
